e425fdf3509cd507ff5d0230803eadf6dc785fc2
Harsh Sabhaya <harshs@mobifyi.com>
2020-11-27 18:33:06 +0530

    fix: StateManage ingredientForm

--- hooks-01/src/components/Ingredients/IngredientForm.js
+++ hooks-01/src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [inputState, setInputState] = useState({title: '', amount: ''})
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +16,29 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input 
+              type="text"
+              id="title" 
+              value={inputState.title} 
+              onClick={(event) =>{ 
+                const newTitle = event.target.value;
+                setInputState((prevInputState) => ({
+                  title: newTitle,
+                  amount: prevInputState.amount
+                }));
+              }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" 
+              value= {inputState.amount}
+              onClick={(event) => {
+                const newAmount = event.target.amount;
+                setInputState(prevInputState => ({
+                  amount: newAmount,
+                  title: prevInputState.title
+                }));
+              }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
